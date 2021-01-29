bab74de6b77820358c27d2d3c77e29ba83570805
Pascal Gerrist <pascalgerrist@pascals-mbp.home>
2021-01-29 01:49:12 +0100

    Changed module structure

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,31 @@
-declare namespace RNStorage {
+/**
+ * Sets item in storage with key
+ * @param {string} key - Storage key
+ * @param data - Data to store
+ * @returns resolves promise when item was set
+ */
 
-    /**
-     * Sets item in storage with key
-     * @param {string} key - Storage key
-     * @param data - Data to store
-     * @returns resolves promise when item was set
-     */
+export function set(key: string, data: Object): Promise<void>;
 
-    export function set(key: string, data: Object): Promise<void>;
+/**
+ * Returns 'key' change in storage
+ * @param {string} key - Storage key
+ * @returns Promise that returns storage content of `key`. If it doesn't exists it'll return null
+ */
 
-    /**
-     * Returns 'key' change in storage
-     * @param {string} key - Storage key
-     * @returns Promise that returns storage content of `key`. If it doesn't exists it'll return null
-     */
+export function get(key: string): Promise<any>;
 
-    export function get(key: string): Promise<any>;
+/**
+ * Watches 'key' change in storage
+ * @param {string} conf.key - Storage key
+ * @param {string} conf.onChange - Callback when change detected
+ * @param {string} conf.error - Error callback
+ * @param {string} [conf.onInit] - Fire callback when watch is initiated
+ */
 
-    /**
-     * Watches 'key' change in storage
-     * @param {string} conf.key - Storage key
-     * @param {string} conf.onChange - Callback when change detected
-     * @param {string} conf.error - Error callback
-     * @param {string} [conf.onInit] - Fire callback when watch is initiated
-     */
-
-    export function watch(conf: {
-        key: string,
-        onChange: Function,
-        error: Function,
-        onInit?: boolean
-    }): void;
-}
+export function watch(conf: {
+    key: string,
+    onChange: Function,
+    error: Function,
+    onInit?: boolean
+}): void;
